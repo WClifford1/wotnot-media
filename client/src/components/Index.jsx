410081522c38ece830f59d01fd54734781d6537b
fari3001 <fari3001_14@hotmail.com>
2019-07-11 18:45:40 +1000

    Initialised Navbar

--- client/src/components/Index.jsx
+++ client/src/components/Index.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
-import { Content, Container } from 'bloomer'
+import { Content, Navbar } from 'bloomer'
+import { NavbarBrand } from 'bloomer/lib/components/Navbar/NavbarBrand';
 
 export default function index() {
     return (
         <div>
-            <Container>
-                <Content>
-                    <h1>Hello World</h1>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum eius, voluptas quo tempora corrupti hic corporis aperiam sapiente nam, animi cumque, aspernatur vero nostrum deleniti. Culpa possimus ipsa fugiat id!</p>
-                </Content>
-            </Container>
+            <Navbar style={{ border: 'solid 1px #00D1B2', margin: '0' }}>
+                <NavbarBrand></NavbarBrand>
+            </Navbar>
+
+
+            <Content>
+                <h1>Hello World</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum eius, voluptas quo tempora corrupti hic corporis aperiam sapiente nam, animi cumque, aspernatur vero nostrum deleniti. Culpa possimus ipsa fugiat id!</p>
+            </Content>
         </div>
+
     )
 }
