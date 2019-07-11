d8fe77b4f43c934b8c247c053f50e6b6aa605800
William Clifford <williamdavidclifford@gmail.com>
2019-07-11 20:03:29 +1000

    basic setup

--- server.js
+++ server.js
@@ -7,8 +7,13 @@ const enquiry = require('./config/keys')
 const app = express()
 
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 const db = require('./config/keys').mongoURI
+
+app.get('/api/hello', (req, res) => {
+    res.send({express: 'Hello from Express'})
+})
 
 mongoose.connect(db)
 .then(() => console.log('Connected to MongoDB Atlas'))
