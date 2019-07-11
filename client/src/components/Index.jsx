329c42dc1a2e5e1eb46ffeb2ad7381077b75cc4b
fari3001 <fari3001_14@hotmail.com>
2019-07-11 19:47:25 +1000

    Switched CSS Frameworks

--- client/src/components/Index.jsx
+++ client/src/components/Index.jsx
@@ -1,20 +1,16 @@
 import React from 'react'
-import { Content, Navbar } from 'bloomer'
-import { NavbarBrand } from 'bloomer/lib/components/Navbar/NavbarBrand';
+import { Button } from 'reactstrap';
 
 export default function index() {
     return (
-        <div>
-            <Navbar style={{ border: 'solid 1px #00D1B2', margin: '0' }}>
-                <NavbarBrand></NavbarBrand>
-            </Navbar>
+       
 
-
-            <Content>
+            <div>
                 <h1>Hello World</h1>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum eius, voluptas quo tempora corrupti hic corporis aperiam sapiente nam, animi cumque, aspernatur vero nostrum deleniti. Culpa possimus ipsa fugiat id!</p>
-            </Content>
-        </div>
+                <Button color="danger">Danger!</Button>
+            </div>
+    
 
     )
 }
