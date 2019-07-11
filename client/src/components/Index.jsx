5d59984e2ef7148f45d2c4c1bdbb0daaaf62d7b5
fari3001 <fari3001_14@hotmail.com>
2019-07-11 18:23:16 +1000

    Added Bulma Package

--- client/src/components/Index.jsx
+++ client/src/components/Index.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Content, Container } from 'bloomer'
 
 export default function index() {
     return (
         <div>
-            <h1>Hello World</h1>
+            <Container>
+                <Content>
+                    <h1>Hello World</h1>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum eius, voluptas quo tempora corrupti hic corporis aperiam sapiente nam, animi cumque, aspernatur vero nostrum deleniti. Culpa possimus ipsa fugiat id!</p>
+                </Content>
+            </Container>
         </div>
     )
 }
