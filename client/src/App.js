cbdc792e227c6c3802b97b4d0e79361bed121148
fari3001 <fari3001_14@hotmail.com>
2019-07-12 11:07:11 +1000

    Removed frameworks

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Index from './components/Index';
 // import Nav from './components/Not';
 import Not from './components/Not';
+import Hero from './components/Hero'
 
 function App() {
   return (
     <React.Fragment>
       <Not />
+      <Hero />
     </React.Fragment>
   );
 }
--- client/src/components/Not.jsx
+++ client/src/components/Not.jsx
@@ -29,7 +29,7 @@ export default class Not extends React.Component {
   render() {
     return (
       <div>
-        <Navbar color="light" light expand="md">
+        <Navbar color="info" light expand="md">
           <NavbarBrand href="/">reactstrap</NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
