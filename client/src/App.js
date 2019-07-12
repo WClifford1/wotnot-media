6a0fe81c55e92a19596aec7d9c1477208302bc2a
fari3001 <fari3001_14@hotmail.com>
2019-07-12 11:24:01 +1000

    Removed unused components

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import './App.css';
-import Index from './components/Index';
-// import Nav from './components/Not';
-import Not from './components/Not';
-import Hero from './components/Hero'
 
 function App() {
   return (
     <React.Fragment>
-      <Not />
-      <Hero />
+    
     </React.Fragment>
   );
 }
