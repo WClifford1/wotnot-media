eba3b52fb2397f5add018a4cf4f2b96fb7cb8013
fari3001 <fari3001_14@hotmail.com>
2019-07-12 11:21:33 +1000

    Removed bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import EnquiryForm from './components/EnquiryForm';
 
 ReactDOM.render(<div><App /><EnquiryForm /></div>, document.getElementById('root'));
