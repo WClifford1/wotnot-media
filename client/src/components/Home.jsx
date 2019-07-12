16e1fadcb421d8e6903e4d913643170314b2ab39
fari3001 <fari3001_14@hotmail.com>
2019-07-12 13:18:34 +1000

    Added classnames

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -6,12 +6,13 @@ export default function Home() {
             <div className="nav">
                 <h1 className="logo">WotNot Media</h1>
                 <ul class="nav-items">
-                    <li><a href="default.asp">Projects</a></li>
-                    <li><a href="news.asp">Services</a></li>
-                    <li><a href="about.asp">FAQ</a></li>
-                    <li><a href="contact.asp">Contact</a></li>
+                    <li><a className="hov" href="default.asp">Projects</a></li>
+                    <li><a className="hov" href="news.asp">Services</a></li>
+                    <li><a className="hov" href="about.asp">FAQ</a></li>
+                    <li><a className="hov" href="contact.asp">Contact</a></li>
                 </ul>
             </div>
+            <hr/>
         </div>
     )
 }
