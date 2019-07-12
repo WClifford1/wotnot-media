c27e7c1fa5e3838e4086fa3f4a48771664bfbaa2
fari3001 <fari3001_14@hotmail.com>
2019-07-12 19:14:09 +1000

    Home Page nav and hero

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 // import EnquiryForm from './components/EnquiryForm';
 import Home from './components/Home';
+import HeroImage from './components/HeroImage';
 
 function App() {
   return (
     <React.Fragment>
       <Home />
+      <HeroImage />
     </React.Fragment>
   );
 }
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,17 +2,14 @@ import React from 'react'
 
 export default function Home() {
     return (
-        <div>
             <div className="nav">
-                <h1 className="logo" style={{ color:"rgb(46, 45, 45)"}}>WotNot Media</h1>
+                <h1 className="logo">WotNot Media</h1>
                 <ul className="nav-items">
                     <li><a className="hov" href="default.asp">Projects</a></li>
                     <li><a className="hov" href="news.asp">Services</a></li>
                     <li><a className="hov" href="about.asp">FAQ</a></li>
                     <li><a className="hov" href="contact.asp">Contact</a></li>
                 </ul>
-            </div>
-            <hr/>
-        </div>
+            </div>    
     )
 }
