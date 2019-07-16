599df1ed4dc70743f6a0750c3f09b18c40d63e57
Joseph <jaymeagalli@gmail.com>
2019-07-16 15:56:02 +1000

    login frontend

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+class Login extends Component {
+    constructor() {
+        super()
+        this.state = {
+            name: "",
+            password: "",
+            errors: {}
+        }
+    }
+
+    onChange = e => {
+        this.setState({ [e.target.id]: e.target.value })
+    }
+
+    onSubmit = e => {
+        e.preventDefault()
+
+        const userData = {
+            name: this.state.name,
+            username: this.state.username,
+            password: this.state.password
+        }
+
+        console.log(userData)
+    }
+
+    render() {
+        const { errors } = this.state
+        
+        return(
+            <div className="container">
+                <div style={{ marginTop: "4rem" }} className="row">
+                    <div className="col s8 offset-s2">
+                        <Link to="/" className="btn-flat waves-effect">
+                            <i className="material-icons
+                            left">keyboard_space</i> Back to
+                            home
+                        </Link>
+                        <div className="col s12" style={{ paddingLeft: 
+                        "11.250px" }}>
+                            <h4>
+                                <b>Login</b> below
+                            </h4>
+                            <p className="grey-text text-darken-1">
+                                Dont have an account? <Link
+                                to="/register"></Link>
+                            </p>
+                        </div>
+                        <form noValidate onSubmit={this.onSubmit}>
+                            <div className="input-field col s12">
+                                <input
+                                onChange={this.onChange}
+                                value={this.state.name}
+                                error={errors.name}
+                                id="name"
+                                type="text" />
+                                <label htmlFor="name">Name</label>
+                                </div>
+                                <div className="input-field col s12">
+                                    <input
+                                    onChange={this.onChange}
+                                    value={this.state.username}
+                                    error={errors.username}
+                                    id="username"
+                                    type="username" />
+                                    <label htmlFor="username">Username</label>
+                                </div>
+                                <div className="input-field col s12">
+                                    <input
+                                    onChange={this.onChange}
+                                    value={this.state.password}
+                                    error={errors.password}
+                                    id="password"
+                                    type="password" />
+                                    <label htmlFor="password">Password`</label>
+                                </div>
+                                <div className="col s12" style={{ paddingLeft:
+                                 "11.250px" }}>
+                                     <button
+                                     style={{
+                                         width: "150px",
+                                         borderRadius: "3px",
+                                         letterSpacing: "1.5px",
+                                         marginTop: "1rem"
+                                     }}
+                                     type="submit"
+                                     className="btn btn-large waves-effect waves-
+                                     light hoverable blue accent-3"
+                                     >
+                                         Login
+                                     </button>
+                                 </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Login
