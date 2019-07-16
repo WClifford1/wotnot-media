cac700f87c3bc88a7f2c7e5dd3a667a5041e6fa1
Joseph <jaymeagalli@gmail.com>
2019-07-16 13:44:50 +1000

    backend register/login

--- server.js
+++ server.js
@@ -6,19 +6,33 @@ const db = require('./config/keys').mongoURI
 const enquiries = require('./routes/api/enquiries')
 const bookings = require('./routes/api/bookings')
 const cors = require('cors')
+const passport = require('passport')
+
+const users = require('./routes/api/users')
+
+
 
 
 app.use(cors())
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: false}))
 app.use('/api/enquiries', enquiries)
 app.use('/api/bookings', bookings)
+//routes
+app.use('/api/users', users)
 
 
 // mongoose.connect(db)
 mongoose.connect('mongodb://localhost/wotnotmedia')
 .then(() => console.log('Connected to Localhost'))
 .catch(err => console.log(err))
+
+//passport middleware
+app.use(passport.initialize())
+
+//passport config
+require('./config/passport')(passport)
+
 
 
 const PORT = process.env.PORT || 4000
