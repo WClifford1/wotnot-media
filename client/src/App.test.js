5716e89641e3248332fd012440fb3012608c129a
fari3001 <fari3001_14@hotmail.com>
2019-07-16 21:40:36 +1000

    More testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import Service1 from './components/Service1';
 import Home from './components/Home';
+import HeroImage from './components/HeroImage';
+import Guarantee from './components/Guarantee';
+import EnquiryForm from './components/EnquiryForm';
+import BookingForm from './components/BookingForm';
 
 import { shallow } from 'enzyme'
+
 
 
 // Service 1 
@@ -36,20 +41,115 @@ describe('<Home />', () => {
     expect(wrapper.contains(welcome)).toEqual(true);
   });
   
-  it('renders a `.serv-title`', () => {
+  it('renders a `.logo`', () => {
     const wrapper = shallow(<Home />);
-    expect(wrapper.find('.serv-title')).to.have.lengthOf(1);
+    expect(wrapper.find('.logo')).to.have.lengthOf(1);
   });
 
   it('renders children when passed in', () => {
     const wrapper = shallow((
       <Home>
-        <div className="serv-cont" />
+        <div className="nav" />
       </Home>
     ));
-    expect(wrapper.contains(<div className="serv-cont" />)).to.equal(true);
+    expect(wrapper.contains(<div className="nav" />)).to.equal(true);
   });
 });
+
+// HeroImage 
+describe('<HeroImage />', () => {
+  it('Has a HeroImage heading', () => {
+    const wrapper = shallow(<HeroImage />);
+    const welcome = <h1 className="catch-phrase">Lucrative, Innovative, Design Solutions</h1>
+    expect(wrapper.contains(welcome)).toEqual(true);
+  });
+  
+  it('renders a `.hero-pos`', () => {
+    const wrapper = shallow(<HeroImage />);
+    expect(wrapper.find('.hero-pos')).to.have.lengthOf(1);
+  });
+
+  it('renders children when passed in', () => {
+    const wrapper = shallow((
+      <HeroImage>
+        <div className="hero-banner" />
+      </HeroImage>
+    ));
+    expect(wrapper.contains(<div className="hero-banner" />)).to.equal(true);
+  });
+});
+
+
+// Guarantee  
+describe('<Guarantee />', () => {
+  it('Has a Guarantee heading', () => {
+    const wrapper = shallow(<Guarantee />);
+    const welcome = <h2>Delivering tailor made, custom design oriented graphics to your liking</h2>
+    expect(wrapper.contains(welcome)).toEqual(true);
+  });
+  
+  it('renders a `.guar-icons`', () => {
+    const wrapper = shallow(<Guarantee />);
+    expect(wrapper.find('.guar-icons')).to.have.lengthOf(1);
+  });
+
+  it('renders children when passed in', () => {
+    const wrapper = shallow((
+      <Guarantee>
+        <div className="guar-container" />
+      </Guarantee>
+    ));
+    expect(wrapper.contains(<div className="guar-container" />)).to.equal(true);
+  });
+});
+
+// Enquiry  
+describe('<EnquiryForm />', () => {
+  it('Has a EnquiryForm heading', () => {
+    const wrapper = shallow(<EnquiryForm />);
+    const welcome = <h3>Enquiry Form</h3>
+    expect(wrapper.contains(welcome)).toEqual(true);
+  });
+  
+  it('renders a `.form-text`', () => {
+    const wrapper = shallow(<EnquiryForm />);
+    expect(wrapper.find('.form-text')).to.have.lengthOf(1);
+  });
+
+  it('renders children when passed in', () => {
+    const wrapper = shallow((
+      <EnquiryForm>
+        <div className="enq-cont" />
+      </EnquiryForm>
+    ));
+    expect(wrapper.contains(<div className="enq-cont" />)).to.equal(true);
+  });
+});
+
+// BookingForm  
+describe('<BookingForm />', () => {
+  it('Has a BookingForm heading', () => {
+    const wrapper = shallow(<BookingForm />);
+    const welcome = <h3>Booking Form</h3>
+    expect(wrapper.contains(welcome)).toEqual(true);
+  });
+  
+  it('renders a `.book-form`', () => {
+    const wrapper = shallow(<BookingForm />);
+    expect(wrapper.find('.book-form')).to.have.lengthOf(1);
+  });
+
+  it('renders children when passed in', () => {
+    const wrapper = shallow((
+      <BookingForm>
+        <div className="book-cont" />
+      </BookingForm>
+    ));
+    expect(wrapper.contains(<div className="book-cont" />)).to.equal(true);
+  });
+});
+
+
 
 
 // it('renders without crashing', () => {
