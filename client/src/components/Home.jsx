94227cb33c85c2e039122ee8c69cd313d093b3f2
fari3001 <fari3001_14@hotmail.com>
2019-07-18 15:15:59 +1000

    Styled hero btn

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -17,8 +17,11 @@ export default function Home() {
 
             <div className="hero-image">
                 <div className="hero-text">
-                    <h2>Lucrative, Innovative, Design Solutions</h2>
+                    <h1>Lucrative, Innovative, Design Solutions</h1>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga maiores sit voluptates optio odit nihil iusto est sint qui! Exercitationem delectus nihil alias praesentium iste maiores officiis quisquam repellat. Voluptates.</p>
+                    <div className="hbtn-cont">
+                        <button type="button" class="hero-btn">Enquire Today!</button>
+                    </div>
                 </div>
                 <img src={heroimg} className="blob" alt="hoohaa" />
             </div>
