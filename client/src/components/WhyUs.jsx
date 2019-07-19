cd323dee215d75a6b6cc0be478ad048ab0a2016a
fari3001 <fari3001_14@hotmail.com>
2019-07-19 16:13:27 +1000

    New Icons added

--- client/src/components/WhyUs.jsx
+++ client/src/components/WhyUs.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import prog from '../images/prog.svg'
+import revision from '../images/revisions.svg'
+import color from '../images/color.svg'
 
 export default function WhyUs() {
     return (
@@ -6,25 +9,27 @@ export default function WhyUs() {
         <div className="whyus-outer">
             <div className="whyus-cont">
                 <div className="wu-icons">
-                    <i className="fas fa-video fa-3x" aria-hidden="true"></i>
+                    <img src={prog} className="blob" alt="hoohaa" />                    
                     <div className="wu-text">
-                        <h3>Something</h3>
+                        <h3>Dedicated Graphics Application</h3>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, voluptatum?</p>
                     </div>
                 </div>
 
                 <div className="wu-icons">
-                    <i className="fas fa-video fa-3x" aria-hidden="true"></i>
+                    {/* <i className="fas fa-video fa-3x" aria-hidden="true"></i> */}
+                    <img src={revision} className="blob" alt="hoohaa" />                    
                     <div className="wu-text">
-                        <h3>Something</h3>
+                        <h3>Unlimited Revisions</h3>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, voluptatum?</p>
                     </div>
                 </div>
 
                 <div className="wu-icons">
-                    <i className="fas fa-video fa-3x" aria-hidden="true"></i>
+                    {/* <i className="fas fa-video fa-3x" aria-hidden="true"></i> */}
+                    <img src={color} className="blob" alt="hoohaa" />                    
                     <div className="wu-text">
-                        <h3>Something</h3>
+                        <h3>Color Theory</h3>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, voluptatum?</p>
                     </div>
                 </div>
