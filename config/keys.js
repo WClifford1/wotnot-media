37f9af59c095195afa470a237faa85343b8f40b3
William Clifford <williamdavidclifford@gmail.com>
2019-07-22 13:36:24 +1000

    add engines to package.json

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
 module.exports = {
-    ENV = "secretMongoUrl=mongodb+srv://farhaan:farino14.@wot-not-media-elqlb.mongodb.net/test?retryWrites=true&w=majority",
     mongoURI: 'mongodb+srv://farhaan:farino14.@wot-not-media-elqlb.mongodb.net/test?retryWrites=true&w=majority',
     secretOrKey: "e6d3a97d-1fc1-4498-ab34-34d42c946da6"
 }
