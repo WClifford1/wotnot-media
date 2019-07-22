85971288862e0c6612ff21661df92089a5932623
Joseph <jaymeagalli@gmail.com>
2019-07-22 10:32:51 +1000

    Pre-merge

--- models/Booking.js
+++ models/Booking.js
@@ -24,16 +24,16 @@ const Booking = mongoose.model('Booking', new mongoose.Schema({
     }
 }))
 
-function validateBooking(booking){
-    const schema = ({
-        name: Joi.string().min(3).max(20).required(),
-        email: Joi.string().min(5).max(20).required(),
-        booking: Joi.string().min(3).max(100).required(),
+// function validateBooking(booking){
+//     const schema = ({
+//         name: Joi.string().min(3).max(20).required(),
+//         email: Joi.string().min(5).max(20).required(),
+//         booking: Joi.string().min(3).max(100).required(),
         
-    })
+//     })
 
-    return Joi.validate(booking, schema)
-}
+//     return Joi.validate(booking, schema)
+// }
 
 exports.Booking = Booking
 exports.validateBooking = validateBooking
