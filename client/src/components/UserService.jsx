bdfc66038a291276097ecac35c182ffe0fa79531
fari3001 <fari3001_14@hotmail.com>
2019-07-22 13:24:10 +1000

    All changes

--- client/src/components/UserService.jsx
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-export function register(user) {
-    return axios.post('http://localhost:4000/api/users',{
-        name: user.name,
-        password: user.password
-    })
-}
