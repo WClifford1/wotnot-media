e3c13d0945b6937357a38f2d46c3aff65eea80ec
fari3001 <fari3001_14@hotmail.com>
2019-07-22 10:35:09 +1000

    Toggle function

--- client/src/components/Faq.jsx
+++ client/src/components/Faq.jsx
@@ -1,34 +1,40 @@
 import React, { Component } from 'react'
 
 export default class Faq extends Component {
-       constructor(props) {
-           super(props)
-       
-        
-            this.onClick = this.onClick.bind(this);
-        
-       }
-       
+    constructor(props) {
+        super(props)
 
-    onClick (button) {
-            button.addEventListener('click', () => {
-                button.classList.toggle('accordion__button--active')     
-            })
+        this.onClick = this.onClick.bind(this);
+
+
+        this.state = {
+            toggle: false
         }
+    }
+
+
+    onClick(button) {
+        // button.addEventListener('click', () => {
+            this.setState( {
+                toggle: !this.state.toggle
+            })
+            // classList.toggle('accordion__button--active')
+        
+    }
 
     render() {
-    return (
-        <div className="question-cont">
-            <h1>Frequently Asked Questions</h1>
-            <div className="accordion">
-                <button type="button" className="accordion__button" onClick={this.onClick}>Expand Content</button>
-                <div className="accordion__content">
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum neque, tempora totam obcaecati odit expedita vero sunt nobis placeat? Natus?
+        return (
+            <div className="question-cont">
+                <h1>Frequently Asked Questions</h1>
+                <div className="accordion">
+                    <button type="button" className={ this.state.toggle ? "accordion__button" : "accordion__button--active"} onClick={this.onClick}>Expand Content</button>
+                    <div className="accordion__content">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum neque, tempora totam obcaecati odit expedita vero sunt nobis placeat? Natus?
                     </p>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
-    } 
+        )
+    }
 }
