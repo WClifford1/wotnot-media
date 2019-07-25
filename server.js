61057d1d3943739387f4266300f08654f6412049
William Clifford <williamdavidclifford@gmail.com>
2019-07-25 16:18:21 +1000

    add mongodb url, cors to netlify

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-app.use(cors())
-// app.use(cors({origin: "https://festive-cori-46ce58.netlify.com"}))
-require('./startup/db')();
+app.use(cors({origin: "https://festive-cori-46ce58.netlify.com"}))
 require('./startup/routes')(app)
 require('dotenv').config()
-
 
 if (!process.env.jwtPrivateKey){
     throw new Error('FATAL ERROR: jwtPrivateKey is not defined.');
 }
 
+require('./startup/db')();
 
 const PORT = process.env.PORT || 4000
 const server = app.listen(PORT, () => console.log(`Server started on port: ${PORT}`))
